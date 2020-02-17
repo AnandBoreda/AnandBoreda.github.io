f972ae21dd170a92d51134bf2e7c345ad94c0e60
AnandBoreda <zz.anand2016@gmail.com>
2020-02-17 21:48:32 +0530

    footer updated for mobile view,  animations improved

--- javascripts/script.js
+++ javascripts/script.js
@@ -10,13 +10,13 @@ function Rskills(reload)
  }
  function formsubgreet()
  {
-   var x = document.forms["cont-form"]["name"].value;
-   var y = document.forms["cont-form"]["email"].value;
+   var name = document.forms["cont-form"]["name"].value;
+   var mail = document.forms["cont-form"]["email"].value;
    
-  if (x != "" && y != "") 
+  if (name != "" && mail != "") 
   {
-    alert('Thanks for your interest, ' + x +'! \n we will get back to you at '+y)
-    document.getElementById('contact').innerHTML='\n Thank you for contacting us, '+x+'.'+'\n we will reach you at \n'+y;
+    alert('Thanks for your interest, ' + name +'! \n we will get back to you at '+mail)
+    document.getElementById('contact').innerHTML=' Thank you for contacting us, '+name+'!'+' <br/>  we will reach you at  '+mail;
   }
 
  }
