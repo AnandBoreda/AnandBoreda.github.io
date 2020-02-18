c3c0c7b7585a58439af4bb7f388f6663241932dc
AnandBoreda <48198394+AnandBoreda@users.noreply.github.com>
2020-02-18 17:11:47 +0530

    alert replaced with modal

--- javascripts/script.js
+++ javascripts/script.js
@@ -10,14 +10,15 @@ function Rskills(reload)
  }
  function formsubgreet()
  {
-   var name = document.forms["cont-form"]["name"].value;
-   var mail = document.forms["cont-form"]["email"].value;
-   
+   let name = document.forms["cont-form"]["name"].value;
+   let mail = document.forms["cont-form"]["email"].value;
+
   if (name != "" && mail != "") 
   {
-    alert('Thanks for your interest, ' + name +'! \n we will get back to you at '+mail)
+    // alert('Thanks for your interest, ' + name +'! \n we will get back to you at '+mail)
+    document.getElementById('modal-name').innerHTML=name+'!';
+    document.getElementById('modal-mail').innerHTML=mail;
     document.getElementById('contact').innerHTML=' Thank you for contacting us, '+name+'!'+' <br/>  we will reach you at  '+mail;
     document.getElementById('contact').style='text-align:center;padding:50px 0px;font-weight:bolder;'
   }
-
  }
