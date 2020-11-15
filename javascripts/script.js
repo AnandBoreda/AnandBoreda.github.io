db1abf429b5ea4bc5887d2e5e2bc6daa183c1c86
anandboreda <zz.anand2016@gmail.com>
2020-11-15 15:48:25 +0530

    update

--- javascripts/script.js
+++ javascripts/script.js
@@ -12,16 +12,23 @@
     document.getElementById('contact').style='text-align:center;padding:50px 0px;font-weight:bolder;'
   }
  }
+ function verify(){
+  let name = document.forms["cont-form"]["name"].value;
+  let mail = document.forms["cont-form"]["email"].value;
+  if (name == "" || mail == ""){
+    document.getElementById("mbody").innerHTML="Please enter valid details";
+  } 
+ }
 function menutoggle(){
   var menu = document.getElementById('Navbar');
   menu.classList.remove('show');
 }
 
 
-// function age() {
-//   var d = new Date();
-//   var y = d.getFullYear();
-//   var age = y - 1998;
-//   document.getElementById("anand-age").innerHTML = age;
-// }
-// age();
+function age() {
+  var d = new Date();
+  var y = d.getFullYear();
+  var age = y - 1998;
+  document.getElementById("anand_age").innerHTML = age;
+}
+age();
