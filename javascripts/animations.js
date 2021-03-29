e56f696f384dcfab18c91a90b6ef482fe92d4144
anandboreda <zz.anand2016@gmail.com>
2021-03-29 19:41:14 +0530

    PWA - install banner

--- javascripts/animations.js
+++ javascripts/animations.js
@@ -1,102 +1,101 @@
 class TypeWriter {
-    constructor(txtElement, words, wait = 3000) {
-      this.txtElement = txtElement;
-      this.words = words;
-      this.txt = '';
-      this.wordIndex = 0;
-      // this.wait = parseInt(wait, 10);
-      this.wait = 1500;
-      this.type();
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    // this.wait = parseInt(wait, 10);
+    this.wait = 1500;
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if (this.isDeleting) {
+      // Remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 100;
+
+    if (this.isDeleting) {
+      typeSpeed /= 4;
+    }
+
+    // If word is complete
+    if (!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      console.log(this.wait);
+      // Set delete to true
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
       this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 500;
     }
-  
-    type() {
-      // Current index of word
-      const current = this.wordIndex % this.words.length;
-      // Get full text of current word
-      const fullTxt = this.words[current];
-  
-      // Check if deleting
-      if(this.isDeleting) {
-        // Remove char
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-      } else {
-        // Add char
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-      }
-  
-      // Insert txt into element
-      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-  
-      // Initial Type Speed
-      let typeSpeed = 100;
-  
-      if(this.isDeleting) {
-        typeSpeed /= 4;
-      }
-  
-      // If word is complete
-      if(!this.isDeleting && this.txt === fullTxt) {
-        // Make pause at end
-        typeSpeed = this.wait;
-        console.log(this.wait)
-        // Set delete to true
-        this.isDeleting = true;
-      } else if(this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        // Move to next word
-        this.wordIndex++;
-        // Pause before start typing
-        typeSpeed = 500;
-      }
-  
-      setTimeout(() => this.type(), typeSpeed);
-    }
+
+    setTimeout(() => this.type(), typeSpeed);
   }
-  
-  
-  // Init On DOM Load
-  document.addEventListener('DOMContentLoaded', init);
-  
-  // Init App
-  function init() {
-    const txtElement = document.querySelector('.txt-type');
-    const words = JSON.parse(txtElement.getAttribute('data-words'));
-    const wait = txtElement.getAttribute('data-wait');
-    // Init TypeWriter
-    new TypeWriter(txtElement, words, wait);
-  }
+}
+
+// Init On DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
 
 // animejs
 
 anime({
-    targets:'.navbar',
-    translateY:[-300, 0],
-    delay: 4300
-})
+  targets: ".navbar",
+  translateY: [-300, 0],
+  delay: 4300,
+});
 anime({
-    targets:'.portfolio',
-    translateY:[-300, 0],
-    delay: 5000
-})
+  targets: ".portfolio",
+  translateY: [-300, 0],
+  delay: 5000,
+});
 anime({
-    targets: '.dp',
-    scale:[0,1],
-    delay:5500
-  })
+  targets: ".dp",
+  scale: [0, 1],
+  delay: 5500,
+});
 anime({
-    targets:'.name',
-    // translateY:[-300, 0],
-    opacity:[0,1],
-    duration:2000,
-    delay: 6000
-})
+  targets: ".name",
+  // translateY:[-300, 0],
+  opacity: [0, 1],
+  duration: 2000,
+  delay: 6000,
+});
 // animate on scroll (AOS)
-    // AOS.init();
+// AOS.init();
 
 AOS.init({
-    offset: 150, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    mirror: true,
-    duration: 600 // values from 0 to 3000, with step 50ms
-  });
+  offset: 150, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  mirror: true,
+  duration: 600, // values from 0 to 3000, with step 50ms
+});
